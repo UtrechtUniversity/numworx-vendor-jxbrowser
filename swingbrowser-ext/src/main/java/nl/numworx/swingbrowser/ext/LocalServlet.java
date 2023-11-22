918f2f563a9a42475811e8c9a65e5a72eeb31795
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2023-11-22 09:35:46 +0100

    update endpoint

--- swingbrowser-ext/src/main/java/nl/numworx/swingbrowser/ext/LocalServlet.java
+++ swingbrowser-ext/src/main/java/nl/numworx/swingbrowser/ext/LocalServlet.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.Reader;
 import java.io.StringReader;
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -90,9 +91,10 @@ public class LocalServlet extends HttpServlet {
 					}
 					int port = req.getServerPort();
 					String host = req.getScheme() + "://" + req.getServerName() + ":" + port;
-					
-					scorm.put("dme.oauth.endpoint", host + "/dwo/saml/login");
-				    api.SetValue("dme.oauth.redirect_uri", host + "/dwo/oauth2/login3.jsp"); // FIXME
+					URI u = URI.create(api.GetValue("dme.oauth.endpoint"));
+					scorm.put("dme.oauth.endpoint", host + u.getRawPath());
+					u = URI.create(api.GetValue("dme.oauth.redirect_uri"));
+				    api.SetValue("dme.oauth.redirect_uri", host + u.getRawPath());
 
 				}
 				
